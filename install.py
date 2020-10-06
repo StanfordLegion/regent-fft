3f5c8fca352e3167cc1ca3b0db300c20009d756a
Elliott Slaughter <slaughter@cs.stanford.edu>
2020-10-06 15:25:30 -0700

    Improve security of install script.

--- install.py
+++ install.py
@@ -18,6 +18,15 @@
 from __future__ import print_function
 import argparse, multiprocessing, os, shutil, subprocess
 
+def download(dest_path, url, sha1):
+    dest_dir = os.path.dirname(dest_path)
+    dest_file = os.path.basename(dest_path)
+    subprocess.check_call(['wget', '-O', dest_path, url])
+    shasum = subprocess.Popen(
+        ['shasum', '--check'], stdin=subprocess.PIPE, cwd=dest_dir)
+    shasum.communicate('%s  %s' % (sha1, dest_file))
+    assert shasum.wait() == 0
+
 def driver(thread_count):
     root_dir = os.path.dirname(os.path.realpath(__file__))
 
@@ -32,7 +41,7 @@ def driver(thread_count):
 
     os.remove(fftw_tar)
     shutil.rmtree(fftw_src)
-    subprocess.check_call(['curl', '-O', 'http://www.fftw.org/fftw-3.3.8.tar.gz'], cwd=root_dir)
+    download(fftw_tar, 'http://www.fftw.org/%s.tar.gz' % fftw_basename, '59831bd4b2705381ee395e54aa6e0069b10c3626')
     subprocess.check_call(['tar', 'xfz', fftw_tar], cwd=root_dir)
     os.mkdir(fftw_build)
     subprocess.check_call([os.path.join(fftw_src, 'configure'), '--enable-openmp', '--disable-mpi', '--enable-shared', '--with-pic', '--prefix=%s' % fftw_install], cwd=fftw_build)
